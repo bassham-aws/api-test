b9fe60d849c879f9e10c28305eaa5c4a0f5bb6f0
Rick Bassham <brodrick.bassham@gmail.com>
2018-11-04 10:15:41 -0500

    Update circle config

---
+++ assumerole.sh
@@ -0,0 +1,10 @@
+unset  AWS_SESSION_TOKEN
+
+temp_role=$(aws sts assume-role \
+                    --role-arn "arn:aws:iam::183375172283:role/ecr_admin" \
+                    --role-session-name "vgaltes-prod" \
+                    --profile vgaltes-serverless)
+
+export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
+export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
+export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
