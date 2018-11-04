a167b45093cea0256b6cc770d38e6e981457cc9c
Rick Bassham <brodrick.bassham@gmail.com>
2018-11-04 11:09:13 -0500

    Testing CirclCI

--- deployment/docker-login.sh
+++ deployment/docker-login.sh
@@ -1,9 +1,12 @@
 unset  AWS_SESSION_TOKEN
 
 temp_role=$(aws sts assume-role \
-                    --role-arn "arn:aws:iam::183375172283:role/ecr_admin" \
+                    --role-arn "arn:aws:iam::${AWS_ACCOUNT}:role/ecr_admin" \
                     --role-session-name "circleci")
 
 export AWS_ACCESS_KEY_ID=$(echo $temp_role | jq .Credentials.AccessKeyId | xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $temp_role | jq .Credentials.SecretAccessKey | xargs)
 export AWS_SESSION_TOKEN=$(echo $temp_role | jq .Credentials.SessionToken | xargs)
+
+login="$(aws ecr get-login --no-include-email)"
+${login}
